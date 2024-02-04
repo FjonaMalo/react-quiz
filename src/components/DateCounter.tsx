bba756cced6ec5bc1c0f5e14bd31d55428737f83
fjonamalo <fjonamalo0@gmail.com>
2024-02-04 19:52:04 +0100

    using useReducer hook for states

--- src/components/DateCounter.tsx
+++ src/components/DateCounter.tsx
@@ -1,22 +1,33 @@
-import { useState } from "react";
+import { useReducer, useState } from "react";
+
+const reducer = (state: number, action: { type: string; payload?: any }) => {
+  console.log(state, action);
+  if (action.type === "inc") return state + 1;
+  if (action.type === "dec") return state - 1;
+  if (action.type === "setCount") return action.payload;
+};
 
 const DateCounter = () => {
-  const [count, setCount] = useState(0);
+  //   const [count, setCount] = useState(0);
+  const [count, dispatch] = useReducer(reducer, 0);
   const [step, setStep] = useState(1);
 
   const date = new Date("june 21 2027");
   date.setDate(date.getDate() + count);
 
   const dec = () => {
-    setCount((count) => count - step);
+    dispatch({ type: "dec" });
+    // setCount((count) => count - step);
   };
 
   const inc = () => {
-    setCount((count) => count + step);
+    dispatch({ type: "inc" });
+    // setCount((count) => count + step);
   };
 
   const defineCount = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setCount(Number(e.target.value));
+    // setCount(Number(e.target.value));
+    dispatch({ type: "setCount", payload: Number(e.target.value) });
   };
 
   const defineStep = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -24,7 +35,7 @@ const DateCounter = () => {
   };
 
   const reset = () => {
-    setCount(0);
+    // setCount(0);
     setStep(1);
   };
 
